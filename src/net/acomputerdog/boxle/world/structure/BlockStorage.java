74f19032c4dae2ad24c6e0b9c8cab70732b1a842
warriordog <acomputerdog@gmail.com>
2014-09-25 20:55:48 -0400

    Add comments to BlockStorage

--- src/net/acomputerdog/boxle/world/structure/BlockStorage.java
+++ src/net/acomputerdog/boxle/world/structure/BlockStorage.java
@@ -7,42 +7,87 @@ import net.acomputerdog.boxle.world.Chunk;
  * Class used to hold blocks for a Chunk.
  */
 public class BlockStorage {
+    /**
+     * Array containing powers of two up to 2048
+     */
     private static final int[] POWERS_OF_TWO = new int[]{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
+    //stops at 2048 due to maximum reasonable size for a BlockStorage.  Anything over that (or 64, really) would take an extremely long time to set a block or compress arrays.
 
+    /**
+     * Maximum size of the arrays in this BlockStorage
+     */
     private final int maxSize;
 
+    /**
+     * Array containing blocks
+     */
     private Block[][][] blockArray = new Block[1][1][1];
+
+    /**
+     * Array containing block data values
+     */
     private byte[][][] dataArray = new byte[1][1][1];
 
+    /**
+     * Set to true if block arrays have changed since last compression
+     */
     private boolean hasChanged = false;
 
+    /**
+     * Creates a new BlockStorage
+     *
+     * @param maxSize The maximum size of this BlockStorage.  Cannot be greater than 2048.
+     */
     public BlockStorage(int maxSize) {
-        if (!isPowerOfTwo(maxSize)) {
-            throw new IllegalArgumentException("maxSize must be a power of two!");
-        }
+        if (!isPowerOfTwo(maxSize)) throw new IllegalArgumentException("maxSize must be a power of two!");
         this.maxSize = maxSize;
     }
 
+    /**
+     * Creates a new BlockStorage with a mazSize of 16
+     */
     public BlockStorage() {
         this(Chunk.CHUNK_SIZE);
     }
 
+    /**
+     * Gets the data value of a location
+     * @param x X-location of block
+     * @param y Y-location of block
+     * @param z Z-location of block
+     * @return return the data value of the block
+     */
     public byte getData(int x, int y, int z) {
-        verifyBounds(x, y, z);
+        verifyBounds(x, y, z); //make sure x, y, and z are within bounds.
         int indexX = findIndex(x, dataArray.length);
         int indexY = findIndex(y, dataArray[0].length);
         int indexZ = findIndex(z, dataArray[0][0].length);
         return dataArray[indexX][indexY][indexZ];
     }
 
+    /**
+     * Gets the block at a location
+     * @param x X-location of block
+     * @param y Y-location of block
+     * @param z Z-location of block
+     * @return return the block at the location
+     */
     public Block getBlock(int x, int y, int z) {
-        verifyBounds(x, y, z);
+        verifyBounds(x, y, z); //make sure x, y, and z are within bounds.
         int indexX = findIndex(x, blockArray.length);
         int indexY = findIndex(y, blockArray[0].length);
         int indexZ = findIndex(z, blockArray[0][0].length);
+        //todo return a default block (air?) if null
         return blockArray[indexX][indexY][indexZ];
     }
 
+    /**
+     * Sets a block at a location
+     * @param x X-location of block
+     * @param y Y-location of block
+     * @param z Z-location of block
+     * @param block Block to set.  Cannot be null.
+     */
     public void setBlock(int x, int y, int z, Block block) {
         if (block == null) throw new IllegalArgumentException("Block cannot be null!");
         verifyBounds(x, y, z);
@@ -50,37 +95,65 @@ public class BlockStorage {
         blockArray[x][y][z] = block;
     }
 
+    /**
+     * Sets the data value at a location
+     * @param x X-location of block
+     * @param y Y-location of block
+     * @param z Z-location of block
+     * @param data The data value to set
+     */
     public void setData(int x, int y, int z, byte data) {
         verifyBounds(x, y, z);
         expandArrays();
         dataArray[x][y][z] = data;
     }
 
+    /**
+     * Finds the scaled index of a location.
+     * @param index The real (0 - maxSize) index of the location
+     * @param size The size of the array to scale to
+     * @return Return the scaled index of the location
+     */
     private int findIndex(int index, int size) {
         int scale = maxSize / size;
         return index / scale;
     }
 
+    /**
+     * Verifies that the x, y, and z locations are within the bounds of this BlockStorage
+     * @param x X-location
+     * @param y Y-location
+     * @param z Z-location
+     */
     private void verifyBounds(int x, int y, int z) {
         if (x < 0 || x > maxSize || y < 0 || y > maxSize || z < 0 || z > maxSize) {
-            throw new IllegalArgumentException("x, y, or z is out of bounds (" + x + ", " + y + ", " + z + ")!");
+            throw new IllegalArgumentException("An index is out of bounds! (" + x + ", " + y + ", " + z + ")!");
         }
     }
 
-    /*
-    private int findPowerOfTwo(int size) {
-        for (int num : POWERS_OF_TWO) {
-            if (num > maxSize) {
+    /**
+     * Finds a power of two greater than or equal to a number but less than or equal to maxSize
+     *
+     * @param num The number to find
+     * @return return an int that is a power of two greater than or equal to num
+     */
+    private int findPowerOfTwo(int num) {
+        for (int pow : POWERS_OF_TWO) {
+            if (pow > maxSize) {
                 throw new RuntimeException("Could not find a power of two smaller than maxSize!");
             }
-            if (num >= size) {
-                return num;
+            if (pow >= num) {
+                return pow;
             }
         }
-        throw new RuntimeException("Could not find a power of two smaller than size!");
+        throw new RuntimeException("Could not find a power of two smaller than num!");
     }
-    */
 
+    /**
+     * Scan and compress the arrays of this BlockStorage.
+     * Uses many iterations and manual array copies, so is very slow.
+     * Not yet implemented
+     */
     public void compressArrays() {
         if (hasChanged) {
             //compression NYI
@@ -88,6 +161,9 @@ public class BlockStorage {
         }
     }
 
+    /**
+     * Expand the arrays in this BlockStorage to maxSize
+     */
     private void expandArrays() {
         hasChanged = true;
         if (blockArray.length < maxSize) {
@@ -120,42 +196,25 @@ public class BlockStorage {
         }
     }
 
-    /*
-    private void resizeBA(int sizeX, int sizeY, int sizeZ) {
-        //
-        // Incorrect method!
-        // Need to actually scale up values!
-        //
-        Block[][][] newBA = new Block[sizeX][sizeY][sizeZ];
-        if (blockArray.length < size) {
-            int newScale = size / blockArray.length;
-            Block[][][] newBA = new Block[size][blockArray[0].length][blockArray[1].length];
-            for (int x = 0; x < blockArray.length; x++) {
-                int indexX = x * newScale;
-                for (int y = 0; y < blockArray[0].length; y++) {
-                    int indexY = y * newScale;
-                    for (int z = 0; z < blockArray[1].length; z++) {
-                        int indexZ = z * newScale;
-                        newBA[x][y][z] = blockArray[x][y][z];
-                    }
-                }
-            }
-        } else {
-
-        }
-
-    }
-*/
-
-    private static boolean isPowerOfTwo(int size) {
-        for (int num : POWERS_OF_TWO) {
-            if (num == size) {
+    /**
+     * Checks if a number is a power of two
+     *
+     * @param num The number to check
+     * @return return true if the number is a power of two, false otherwise
+     */
+    private static boolean isPowerOfTwo(int num) {
+        for (int pow : POWERS_OF_TWO) {
+            if (pow == num) {
                 return true;
             }
         }
         return false;
     }
 
+    /**
+     * Run a test of BlockStorage
+     * @param args Arguments.  Not used.
+     */
     public static void main(String[] args) {
         BlockStorage storage = new BlockStorage(Chunk.CHUNK_SIZE);
         Block block1 = new Block();
