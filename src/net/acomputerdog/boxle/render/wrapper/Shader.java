61c7e26dab46e139a6d9b83044e15620fd5a32e2
warriordog <acomputerdog@gmail.com>
2014-10-05 10:26:05 -0400

    Add comments to Shader and ShaderProgram

--- src/net/acomputerdog/boxle/render/wrapper/Shader.java
+++ src/net/acomputerdog/boxle/render/wrapper/Shader.java
@@ -6,16 +6,47 @@ import net.acomputerdog.core.logger.CLogger;
 import static org.lwjgl.opengl.ARBShaderObjects.*;
 import static org.lwjgl.opengl.GL11.GL_FALSE;
 
+/**
+ * Wrapper around a GL shader
+ */
 public class Shader {
+    /**
+     * Logger for use in shaders and shader programs
+     */
     static final CLogger LOGGER = new CLogger("Shaders", true, true);
 
+    /**
+     * The ID of this shader, as returned by the GPU
+     */
     private int shaderID = 0;
+
+    /**
+     * Set to true when the shader is compiled
+     */
     private boolean isCompiled = false;
 
+    /**
+     * The render engine that owns this Shader
+     */
     private final RenderEngine renderEngine;
+
+    /**
+     * The contents of this shader
+     */
     private final String shaderContents;
+
+    /**
+     * The type of this shader
+     */
     private final int shaderType;
 
+    /**
+     * Creates a new shader
+     *
+     * @param renderEngine   The render engine of this shader
+     * @param shaderContents The source code of this shader
+     * @param shaderType     The type of this shader
+     */
     public Shader(RenderEngine renderEngine, String shaderContents, int shaderType) {
         if (renderEngine == null) {
             throw new IllegalArgumentException("Render engine cannot be null!");
@@ -28,6 +59,9 @@ public class Shader {
         this.shaderType = shaderType;
     }
 
+    /**
+     * Compiles the shader, so that it can be added to a shader program
+     */
     public void compile() {
         if (isCompiled) {
             throw new IllegalStateException("Cannot compile an already compiled shader!");
@@ -48,22 +82,42 @@ public class Shader {
         }
     }
 
+    /**
+     * Gets the log info from any recent GL Shader errors
+     * @return Return the log info from any recent shader errors
+     */
     private String getLogInfo() {
         return glGetInfoLogARB(shaderID, glGetObjectParameteriARB(shaderID, GL_OBJECT_INFO_LOG_LENGTH_ARB));
     }
 
+    /**
+     * Gets the ID of this shader
+     * @return Return the ID of this shader
+     */
     public int getShaderID() {
         return shaderID;
     }
 
+    /**
+     * Gets the render engine of this shader
+     * @return Return the render engine of this shader
+     */
     public RenderEngine getRenderEngine() {
         return renderEngine;
     }
 
+    /**
+     * Gets the shader type
+     * @return Return the type of this shader
+     */
     public int getShaderType() {
         return shaderType;
     }
 
+    /**
+     * Checks if the shader has been compiled
+     * @return Return true if the shader has been compiled, false otherwise
+     */
     public boolean isCompiled() {
         return isCompiled;
     }
--- src/net/acomputerdog/boxle/render/wrapper/ShaderProgram.java
+++ src/net/acomputerdog/boxle/render/wrapper/ShaderProgram.java
@@ -7,15 +7,40 @@ import java.util.Set;
 
 import static org.lwjgl.opengl.ARBShaderObjects.*;
 
+/**
+ * A wrapper for a GL Shader Program
+ */
 public class ShaderProgram {
+    /**
+     * The ID of the "null" program.  Used to disable the active shader.
+     */
     private static final int PROGRAM_NULL = 0;
 
+    /**
+     * The render engine that owns this ShaderProgram
+     */
     private final RenderEngine engine;
+
+    /**
+     * The set of attached shaders.
+     */
     private final Set<Shader> attachedShaders = new HashSet<>();
 
+    /**
+     * True if the shader program has been linked
+     */
     private boolean isLinked = false;
+
+    /**
+     * The ID of this program, as returned by the GPU
+     */
     private int programID = 0;
 
+    /**
+     * Creates a new shader program
+     *
+     * @param engine The render engine of the shader
+     */
     public ShaderProgram(RenderEngine engine) {
         if (engine == null) {
             throw new IllegalArgumentException("Render engine cannot be null!");
@@ -23,6 +48,10 @@ public class ShaderProgram {
         this.engine = engine;
     }
 
+    /**
+     * Adds a shader to this ShaderProgram
+     * @param shader The shader to add. Must be already compiled and use the same render engine.
+     */
     public void addShader(Shader shader) {
         if (isLinked) {
             throw new IllegalStateException("Cannot add a shader to a linked program!");
@@ -39,6 +68,9 @@ public class ShaderProgram {
         attachedShaders.add(shader);
     }
 
+    /**
+     * Links this Shader Program.
+     */
     public void link() {
         if (isLinked) {
             throw new IllegalStateException("Program is already linked!");
@@ -61,26 +93,48 @@ public class ShaderProgram {
         }
     }
 
+    /**
+     * Activates this shader program on successive render actions.
+     */
     public void use() {
         glUseProgramObjectARB(programID);
     }
 
+    /**
+     * Deactivates the shader program
+     */
     public void release() {
         glUseProgramObjectARB(PROGRAM_NULL);
     }
 
+    /**
+     * Gets the log info from any GL errors
+     * @return Return the log info from any GL errors
+     */
     private String getLogInfo() {
         return glGetInfoLogARB(programID, glGetObjectParameteriARB(programID, GL_OBJECT_INFO_LOG_LENGTH_ARB));
     }
 
+    /**
+     * Gets the render engine of this shader program
+     * @return Return the render engine of this shader program
+     */
     public RenderEngine getEngine() {
         return engine;
     }
 
+    /**
+     * Checks if the shader program has been linked
+     * @return Return true if the shader program has been linked, false otherwise
+     */
     public boolean isLinked() {
         return isLinked;
     }
 
+    /**
+     * Gets the ID of this shader program
+     * @return Return the ID of this shader program
+     */
     public int getProgramID() {
         return programID;
     }
