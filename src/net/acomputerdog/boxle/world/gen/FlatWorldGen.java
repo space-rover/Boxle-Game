c879decefecdcc4bf6cc6400987b69971da54fe5
warriordog <acomputerdog@gmail.com>
2014-11-13 15:15:23 -0500

    FlatWorldGen can now have a height specified

--- src/net/acomputerdog/boxle/world/gen/FlatWorldGen.java
+++ src/net/acomputerdog/boxle/world/gen/FlatWorldGen.java
@@ -1,10 +1,19 @@
 package net.acomputerdog.boxle.world.gen;
 
 public class FlatWorldGen extends SimpleWorldGen {
+    private final int height;
+
+    public FlatWorldGen(int height) {
+        this.height = height;
+    }
+
+    public FlatWorldGen() {
+        this(10);
+    }
 
     @Override
     public int getGroundHeight(int x, int y) {
-        return 10;
+        return height;
     }
 
 }
